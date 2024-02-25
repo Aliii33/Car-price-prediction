ea8cbcaa473c5e218f2080be6d7be419b803df9a
Aliii33 <118289045+Aliii33@users.noreply.github.com>
2024-02-25 15:24:02 +0000

    Update application.py

--- application.py
+++ application.py
@@ -1,44 +1,43 @@
-from flask import Flask, render_template, request, redirect
-from flask_cors import CORS, cross_origin
+from flask import Flask,render_template,request,redirect
+from flask_cors import CORS,cross_origin
 import pickle
 import pandas as pd
 import numpy as np
 
-app = Flask(__name__)
-cors = CORS(app)
-model = pickle.load(open('Lasso.pkl', 'rb'))
-car = pd.read_csv('Cleaned dataset.csv')
+app=Flask(__name__)
+cors=CORS(app)
+model=pickle.load(open('Lasso.pkl','rb'))
+car=pd.read_csv('Cleaned dataset.csv')
 
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/',methods=['GET','POST'])
 def index():
-    brand = sorted(car['brand'].unique())
-    model = sorted(car['model'].unique())
-    model_year = sorted(car['model_year'].unique(), reverse=True)
-    fuel_type = car['fuel_type'].unique()
+    companies=sorted(car['brand'].unique())
+    car_models=sorted(car['model'].unique())
+    year=sorted(car['model_year'].unique(),reverse=True)
+    fuel_types=car['fuel_type'].unique()
 
-    brand.insert(0, 'Select Company')
-
-
-    return render_template('index.html', brand=brand, model=model, model_year=model_year, fuel_types=fuel_type)
-
+    companies.insert(0,'Select Company')
+    return render_template('index.html',companies=companies, car_models=car_models, year=year,fuel_types=fuel_types)
 
 @app.route('/predict', methods=['POST'])
 @cross_origin()
 def predict():
     brand = request.form.get('brand')
-
-    model = request.form.get('model')
-    model_year = request.form.get('model_year')
+    model_name = request.form.get('model')
+    model_year = int(request.form.get('model_year'))
     fuel_type = request.form.get('fuel_type')
-    milage = request.form.get('milage')
+    mileage = int(request.form.get('milage'))
 
-    prediction = model.predict(pd.DataFrame(columns=['model', 'brand', 'model_year', 'milage', 'fuel_type'],
-                                            data=np.array([model, brand, model_year, milage, fuel_type]).reshape(1, 5)))
-    print(str(np.round(prediction[0], 2)))
+    # Create a DataFrame with user input
+    user_input = pd.DataFrame([[brand, model_name, model_year, mileage, fuel_type]],
+                              columns=['brand', 'model', 'model_year', 'milage', 'fuel_type'])
 
-    return str(np.round(prediction[0], 2))
+    # Make predictions using the loaded model
+    prediction = model.predict(user_input)
 
+    print(prediction)
 
-if __name__ == '__main__':
+    return str(np.round(prediction[0],2))
+
+if __name__=='__main__':
     app.run()
